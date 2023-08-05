f7886cf2b835420b13edc833fabf7cc39b79bc8c
rohilvagarwal <rohilvagarwal@gmail.com>
2023-08-05 10:37:01 -0400

    hosting online

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from ProjectConstants import *
 import sys
 from Button import Button
@@ -14,7 +16,6 @@ d4 = pygame.image.load('images/d4Logo.svg').convert_alpha()
 scaled_d4 = pygame.transform.scale(d4, (50, 50)).convert_alpha()
 
 #game variables
-GAME_OVER = False
 chipperInvaders = ChipperInvaders(100, SCREEN_HEIGHT / 2, 500, 200)
 
 #game states: menu, kinematics, circularMotion, aboutMe
@@ -40,7 +41,7 @@ def draw_d4():
 
 def draw_menu():
 	global gameState
-	global GAME_OVER
+	#global GAME_OVER
 	screen.fill(backgroundColor)
 	draw_d4()
 
@@ -48,14 +49,15 @@ def draw_menu():
 	draw_text_center(screen, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 10, 70, "Arman Gaming")
 
 	chippers = Button(centerX=SCREEN_WIDTH / 2, centerY=SCREEN_HEIGHT / 2, width=350, height=100, textSize=70, borderSize=30, text="Start Boi")
-	exit = Button(centerX=SCREEN_WIDTH / 2, centerY=SCREEN_HEIGHT - 30, width=100, height=40, textSize=25, borderSize=10, text="Exit")
+	#exit = Button(centerX=SCREEN_WIDTH / 2, centerY=SCREEN_HEIGHT - 30, width=100, height=40, textSize=25, borderSize=10, text="Exit")
 
 	#draw button and check if clicked
 	if chippers.draw_and_check_click(screen):
 		gameState = "Chipper Invaders"
 
-	if exit.draw_and_check_click(screen):
-		GAME_OVER = True
+
+# if exit.draw_and_check_click(screen):
+# 	GAME_OVER = True
 
 
 def draw_chipper_invaders():
@@ -66,34 +68,40 @@ def draw_chipper_invaders():
 	chipperInvaders.draw_static(screen)
 
 
-#game start
-draw_menu()
-pygame.display.update()
-
-frame_count = 0
-frame_rate = 0
-start_time = time.time()
-
-while not GAME_OVER:
-	if gameState == "menu":
-		draw_menu()
-	if gameState == "Chipper Invaders":
-		draw_chipper_invaders()
-
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			GAME_OVER = True
-
-	frame_count += 1
-	if time.time() - start_time > 1:
-		frame_rate = frame_count
-		frame_count = 0
-		start_time = time.time()
-
-	draw_text_left(screen, 5, 10, 10, "FPS: " + str(frame_rate))
-
+async def main():
+	#game start
+	GAME_OVER = False
+	draw_menu()
 	pygame.display.update()
-	clock.tick(FPS)
 
-pygame.quit()
-sys.exit()
+	frame_count = 0
+	frame_rate = 0
+	start_time = time.time()
+
+	while not GAME_OVER:
+		if gameState == "menu":
+			draw_menu()
+		if gameState == "Chipper Invaders":
+			draw_chipper_invaders()
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				GAME_OVER = True
+
+		frame_count += 1
+		if time.time() - start_time > 1:
+			frame_rate = frame_count
+			frame_count = 0
+			start_time = time.time()
+
+		draw_text_left(screen, 5, 10, 10, "FPS: " + str(frame_rate))
+
+		pygame.display.update()
+		clock.tick(FPS)
+		await asyncio.sleep(0)
+
+	pygame.quit()
+	sys.exit()
+
+
+asyncio.run(main())
